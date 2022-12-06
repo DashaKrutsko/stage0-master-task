5199cf15327ff43ae158e73b38bc2eccd0b3e97d
DK <dasha.krutsko@gmail.com>
2022-12-06 15:16:42 +0300

    update

--- src/main/java/com/epam/conditions/BitwiseValuesSwap.java
+++ src/main/java/com/epam/conditions/BitwiseValuesSwap.java
@@ -6,7 +6,6 @@ public class BitwiseValuesSwap {
         first = first ^ second;
         second = first^ second;
         first = second ^ first;
-        System.out.println(first);
-        System.out.println(second);
+        System.out.println(first+"\n"+second);
     }
 }
